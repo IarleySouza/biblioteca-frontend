aefeda0d44b52ba2e6136cea25d97f97808e423f
Iarley Souza <iarleysouza615@gmail.com>
2025-11-17 00:48:09 -0300

    up

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { toast } from "react-toastify"
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080"
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://3.87.116.207:8080"
 
 const api = axios.create({
   baseURL: API_BASE_URL,
